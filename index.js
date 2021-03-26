ead7a6ae5a88cace5d78e3d17aa0e8556d8e76ef
mbish08 <mdrew8@gmail.com>
2021-03-26 14:03:53 -0700

    commit tree d209cd16436528175f375217f8ed947660d51d97

--- index.js
+++ index.js
@@ -9,6 +9,66 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeArrays) {
+    return employeeArrays.map(function(row) {
+        return createEmployeeRecord(row)
+    })
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push( {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push( {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(dateWorked) {
+    let inPunch = this.timeInEvents.find(function(e) {
+        return e.date === dateWorked
+    })
+    let outPunch = this.timeOutEvents.find(function(e) {
+        return e.date === dateWorked
+    })
+    return (outPunch.hour - inPunch.hour) /100
+}
+
+function wagesEarnedOnDate(dateWorked) {
+    let dailyWages = hoursWorkedOnDate.call(this, dateWorked) * this.payPerHour
+    return parseFloat(dailyWages.toString())
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce(function(memo, rec) {
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +79,10 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(employeeArray, firstName) {
+    return employeeArray.find(function(rec) {
+        return rec.firstName === firstName
+    })
 }
